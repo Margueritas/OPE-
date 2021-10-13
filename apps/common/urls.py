d3f9e5bb4aa2bb70cb29c1809a3a37bd3f711f6f
Caio Doiche <cdoiche@gmail.com>
2021-10-12 23:54:27 -0300

    Inclusão de rotas nas urls

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(template_name="login.html",), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page=settings.LOGOUT_REDIRECT_URL), name='logout'),
-    path('painel/', views.painel, name='painel')
+    path('painel/', views.painel, name='painel'),
+    path('clientes/', views.clientes, name='clientes'),
+    path('cardapio/', views.cardapio, name='cardapio'),
+    path('pedidos/', views.pedidos, name='pedidos'),
 ]
